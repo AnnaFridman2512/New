189d9a560322b609544a3b0e2197f078c278e870
Anna-Fridman <annafridman81@gmail.com>
2021-07-13 11:30:01 +0300

    Form.js deleted

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -7,8 +7,8 @@ useEffect(()=>{
     if(localStorage.getItem(userName)) {//if the userName already exist in localstorage (localStorage.getItem is a built in function)
         setUser(userName);
     }else{
-        //localStorage.setItem(userName, 'exists' );//(localStorage.setitem is a built in function)
-        setUser(null);
+
+       setUser(null);
     }
 }, [userName]);//Track changes on "userName" and render this every time "userName" changes
    
