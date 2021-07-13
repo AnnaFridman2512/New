59a5fac0c9ddd8bb5be9aef8d22378e7586b167d
Anna-Fridman <annafridman81@gmail.com>
2021-07-13 18:22:42 +0300

    layout changed

--- src/App.js
+++ src/App.js
@@ -24,25 +24,23 @@ export default function App() {
     setValue(value);//set the value with whatever we got from input
   }
 
-  if(githubUser){
-    return (
-      <div className="App">
-      <h1>Repository list:</h1>
-      {repositories.map(repository => (
-        <div key={repository.id}>
-          <h3>{repository.url}</h3>
-        </div>
-      ))}
-    </div>
-    );
-  }else{
-    return (
+  return (
+    <div className="App">
       <form onSubmit={submit}> {/*run "submit" function, stotre value from input*/}
         <input ref={inputRef} type="text"/>
         <button type="submit">submit</button>
       </form>
+
+     
+      <h1>Repository list:</h1>
+        {repositories.map(repository => (
+        <div key={repository.id}>
+        <h3>{repository.url}</h3>
+        </div>
+      ))}
+    </div>
     );
   }
-}
+
 
 
